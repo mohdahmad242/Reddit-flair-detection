91a32b7a2c12e4b5502e0cbaf8e61e88e85f9325
Mohammad Ahmad <md.ahmad0652@gmail.com>
2020-04-26 05:49:13 +0530

    Some changes done

--- WebApp/app.py
+++ WebApp/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.method == 'GET':
-		return(render_template('main.html'))
+		return(render_template('main.html', result={"result": False}))
     
 	if request.method == 'POST':
 		prediction = predict_url(request.form['url'])
